b7ac8f4f2d05aa98622e23a63d6cb46445c12ba3
Igor Ferraz <83618479+Igor-Ferraz7@users.noreply.github.com>
2025-05-07 11:24:40 -0300

    Update MainFrame.java
    Atualizado a tela principal para inicializar o tema escuro quando for chamada externamente.

--- src/interfaces/MainFrame.java
+++ src/interfaces/MainFrame.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package interfaces;
 
 import com.formdev.flatlaf.intellijthemes.*;
@@ -18,7 +14,7 @@ public class MainFrame extends JFrame {
     private String description;
 
     public MainFrame() {
-        //setUndecorated(true);
+        FlatOneDarkIJTheme.setup();
        
         initComponents();
         
